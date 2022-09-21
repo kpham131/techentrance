9cf82f45b008b2ae06a182b68327aaaf8629be0f
kpham131 <phamkhoa311@gmail.com>
2022-09-21 14:23:36 -0400

    Initial commit

---
+++ src/main/java/com/techentrance/techentrance/TechentranceApplication.java
@@ -0,0 +1,13 @@
+package com.techentrance.techentrance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechentranceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechentranceApplication.class, args);
+	}
+
+}
